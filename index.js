4d8c1eded35c8f9d6dd65fb930035b63f3f7f701
HiYasin <hello.mr.faisal@gmail.com>
2025-01-27 14:01:50 +0600

    verify admin middleware created

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const database = client.db("StaffSync");
+    
     //jwt related api
     //jwt middlewares
     const verifyToken = (req, res, next) => {
@@ -52,10 +52,23 @@ async function run() {
         if (err) {
           return res.status(403).send({ message: 'Invalid token.' });
         }
+
         req.decoded = decoded;
+
+        //console.log(req.decoded.email);
         next();
       })
     }
+
+    const verifyAdmin = (req, res, next) => {
+      if (req.decoded.email !== 'admin@staff.com') {
+      return res.status(403).send({ message: 'Admin access required.' });
+      }
+      //console.log('admin verified');
+      next();
+    }
+
+
 
     //create jwt
     app.post('/jwt', async(req, res) => {
@@ -66,6 +79,7 @@ async function run() {
       res.send({ token: token });
     })
 
+    const database = client.db("StaffSync");
     //Authentication related Apis
     const userCollection = database.collection('users');
     //public api to create user
@@ -107,13 +121,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/users/all', verifyToken, async (req, res) => {
-      console.log(req.decoded);
+    //a
+    app.get('/users/all', verifyToken,verifyAdmin, async (req, res) => {
       //console.log(req.headers);
       const query = { role: { $in: ['employee', 'hr'] } }; const result = await userCollection.find(query).toArray();
       res.send(result);
     });
 
+    //h
     app.patch('/users/verify/:id', verifyToken,  async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id), verified: false };
@@ -122,22 +137,26 @@ async function run() {
       res.send(result);
     });
 
-
-    app.patch('/users/fire/:id', verifyToken,  async (req, res) => {
+    //a
+    app.patch('/users/fire/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id), status: 'running' };
       const update = { $set: { status: 'fired' } };
       const result = await userCollection.updateOne(query, update);
       res.send(result);
     });
-    app.patch('/salary-increment/:id', verifyToken,  async (req, res) => {
+
+    //a
+    app.patch('/salary-increment/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const update = { $set: { salary: req.body.salary } };
       const result = await userCollection.updateOne(query, update);
       res.send(result);
     });
-    app.patch('/users/promote/:id', verifyToken,  async (req, res) => {
+
+    //a
+    app.patch('/users/promote/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id), role: 'employee' };
       const update = { $set: { role: 'hr' } };
@@ -145,6 +164,7 @@ async function run() {
       res.send(result);
     });
 
+    //h
     const payrollCollection = database.collection('payroll');
     app.post('/pay', verifyToken,  async (req, res) => {
       const user = req.body;
@@ -158,6 +178,7 @@ async function run() {
       }
     });
     
+    //h&u
     app.get('/payroll', verifyToken,   async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -165,11 +186,14 @@ async function run() {
       res.send(result);
     });
 
+    //h&a
     app.get('/payment', verifyToken,  async (req, res) => {
       const result = await payrollCollection.find().toArray();
       res.send(result);
     });
 
+
+    //h&a
     app.patch('/payment/:id', verifyToken,  async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id), status: 'unpaid' };
@@ -178,6 +202,7 @@ async function run() {
       res.send(result);
     });
 
+    //u
     //User task data store, update and delete
     const taskCollection = database.collection('work-sheet');
     app.post('/work-sheet', verifyToken, async (req, res) => {
@@ -186,6 +211,7 @@ async function run() {
       res.send(result);
     });
 
+    //u
     app.get('/work-sheet', verifyToken, async (req, res) => {
       const email = req.query.email;
       //console.log(email);
@@ -194,12 +220,14 @@ async function run() {
       res.send(result);
     });
     
+    //h
     app.get('/work-sheets', verifyToken, async (req, res) => {
       //console.log('Hit');
       const result = await taskCollection.find().toArray();
       res.send(result);
     });
 
+    //u
     app.put('/work-sheet/:id', verifyToken, async (req, res) => {
       //console.log("put request");
       const id = req.params.id;
@@ -210,6 +238,7 @@ async function run() {
       res.send(result);
     });
 
+    //u
     app.delete('/work-sheet/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
